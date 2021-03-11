cb742f0cb448a720eb8a10fb5e431da57dec28ad
Kira <katdiemin@gmail.com>
2021-03-11 16:32:54 -0500

    create character stuff

--- frontend/src/character/character.ts
+++ frontend/src/character/character.ts
@@ -5,7 +5,7 @@ export class Character {
     dex:number;
     speed:number;
     image:string;
-    constructor(name:string,strength:number,life:number,dex:number,speed:number,image:string){
+    constructor(name:string = "",strength:number = 0,life:number = 0,dex:number = 0,speed:number = 0,image:string = ""){
         this.name= name;
         this.strength= strength;
         this.life = life;
@@ -13,4 +13,9 @@ export class Character {
         this.speed= speed;
         this.image= image;
     }
+
+    takeDamage(damage:number){
+        this.life -= damage;
+    }
+
 }
